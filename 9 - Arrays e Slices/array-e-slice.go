a7cdd9f91001836263a346eb278665062ff5b92b
jesfernandes <jessika.fernandes@mercadolivre.com>
2022-02-16 00:19:26 -0300

    arrays intern

--- 9 - Arrays e Slices/array-e-slice.go
+++ 9 - Arrays e Slices/array-e-slice.go
@@ -33,4 +33,25 @@ func main() {
 
 	array2[1] = "Posição Alterada"
 	fmt.Println(slice2)
+
+	// Arrays Internos (como é construido dentro do slice)
+	fmt.Println("----------")
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // tamanho
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
+	// lembrando que sempre que o slice chega ao tamanho limite, ele dobra de tamanho
+
 }
