67815c7202c79c928d3f6a62d21a0ac0281163ad
jesfernandes <jessika.fernandes@mercadolivre.com>
2022-04-09 20:53:29 -0300

    add concorrencia

---
+++ 18 - Concorrência/goroutines.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//Concorrência != Paralelismo
+	go escrever("Olá mundo!") //goroutine, executa essa função junto com as outras funções simultaneamente
+	escrever("Programando em Go!")
+}
+
+func escrever(texto string) {
+	for {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
+	}
+}
+
+// Pode-se ter várias goroutines
+// Para implementar, deve ser usado o comando "go" antes do metódo a ser executado.
